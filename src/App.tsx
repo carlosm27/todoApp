ef080a54474d07dd9fd5b9c00a080641ac0fe743
Carlos Armando Marcano Vargas <cm.games2704@gmail.com>
2023-04-16 16:45:01 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-//import Get from './components/Get'
-import { Form } from './components/Form'
+
 import { Route, Routes, BrowserRouter } from 'react-router-dom'
 import GetTasks from './pages/listTasks'
 import './App.css'
-import { FormPage } from './pages/createTask'
-import { Update } from './pages/updateTask'
-import { DeleteTask } from './components/RemoveTask'
+import { FormPage } from './components/CreateTask'
+import { Update } from './components/UpdateTask'
+import AppTask from './components/Task'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <div className="App">
     
     
-    <BrowserRouter>
-      
-      <Routes>
-            <Route path="/create" element={<FormPage/>}/>
-            <Route path="/update/:id" element={<Update/>}/>
-            <Route path="/" element={<GetTasks/>}/>
-
-        </Routes>
-    </BrowserRouter>
+    <AppTask/>
       
         
       
