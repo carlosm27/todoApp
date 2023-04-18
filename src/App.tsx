244c55efba6d5a22609271c603920f028b9c0360
Carlos Armando Marcano Vargas <cm.games2704@gmail.com>
2023-04-17 20:24:16 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
 
-import { Route, Routes, BrowserRouter } from 'react-router-dom'
-import GetTasks from './pages/listTasks'
+
 import './App.css'
-import { FormPage } from './components/CreateTask'
-import { Update } from './components/UpdateTask'
+
 import AppTask from './components/Task'
 
 function App() {
