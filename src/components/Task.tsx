6c8beed65e035401dac0e1c397567380a9058af1
Carlos Armando Marcano Vargas <cm.games2704@gmail.com>
2023-04-17 20:24:31 -0400

    Update Task.tsx

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -70,32 +70,40 @@ const AppTask: React.FC = () => {
         <div>
           <h1>Tasks</h1>
           <h2>Add Task</h2>
-          <form onSubmit={(e) => e.preventDefault()}>
-            <input
-              type="text"
-              value={task}
-              onChange={(e) => setTask(e.target.value)}
-            />
-            <button onClick={() => handleAddTask()}>Add</button>
-          </form>
-          <ul>
+              <form onSubmit={(e) => e.preventDefault()}>
+                <input
+                  type="text"
+                  value={task}
+                  onChange={(e) => setTask(e.target.value)}
+                />
+                <button onClick={() => handleAddTask()}>Add</button>
+              </form>
+          <table>
+            <tr>
+              <th>Task</th>
+              <th>Actions</th>
+            </tr>
+          
+          
             {tasks.map((task) => (
-              <li key={task.id}>
-                <span
+              <tr key={task.id}>
+                  <span
                   style={{
                     textDecoration: task.completed ? "line-through" : "none",
                   }}
                   onClick={() => handleToggleCompleted(task.id)}
                 >
+                
                   {task.task}
-                </span>
-                <button onClick={() => handleDeleteTask(task.id)}>Delete</button>
-              </li>
+                  </span>  
+                  <td><button onClick={() => handleDeleteTask(task.id)}>Delete</button></td>
+              </tr>
             ))}
-          </ul>
           
+          </table>
           
         </div>
+        
       );
 };
 
